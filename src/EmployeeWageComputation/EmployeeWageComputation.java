0640290c51826d0b75303e4642a597c1bf300fea
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-03-04 17:07:25 +0530

    Calculate employee daily wage

--- src/EmployeeWageComputation/EmployeeWageComputation.java
+++ src/EmployeeWageComputation/EmployeeWageComputation.java
@@ -8,6 +8,10 @@ public class EmployeeWageComputation {
 
         Random ran = new Random();
         int empCheck = ran.nextInt(2);
+        int WagePerHrs = 20;
+        int FullDayHrs = 8;
+
+        int dailyWage=0;
 
         switch (empCheck){
             case 0:
@@ -15,6 +19,8 @@ public class EmployeeWageComputation {
                 break;
             case 1:
                 System.out.println("Employee is Present");
+                dailyWage = WagePerHrs*FullDayHrs;
+                System.out.println("Employee Daily Wage is :"+dailyWage);
                  break;
         }
 
