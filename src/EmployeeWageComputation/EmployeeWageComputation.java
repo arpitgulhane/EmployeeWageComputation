40e6167e09d08bf64f88f127ddff9cfab1688709
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-03-04 18:09:58 +0530

    Create class and methd

--- src/EmployeeWageComputation/EmployeeWageComputation.java
+++ src/EmployeeWageComputation/EmployeeWageComputation.java
@@ -6,13 +6,23 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("   Welcome to Employee Wage Computation  ");
 
-        int WagePerHrs = 20;
-        int FullDayHrs = 8;
-        int PartTimeHrs =4;
 
-        int dailyWage=0;
-        int month = 20;
-        int monthWage = 20;
+        Employee emp=new Employee();
+        emp.calculate();
+
+
+    }
+}
+
+class Employee{
+    int WagePerHrs = 20;
+    int FullDayHrs = 8;
+    int PartTimeHrs =4;
+
+    int dailyWage=0;
+    int month = 20;
+    int monthWage = 20;
+    public void calculate(){
 
         int totalWorkingHour=1 , day=1;
 
@@ -20,36 +30,34 @@ public class EmployeeWageComputation {
         while (day<=20 || totalWorkingHour<=100){
             Random ran = new Random();
             int empCheck = ran.nextInt(3);
-        switch (empCheck){
-            case 0:
-                System.out.println(" Employee is Absent");
-                day=day+1;
-                break;
-            case 1:
-                System.out.println("Employee is Present");
-                dailyWage = WagePerHrs*FullDayHrs+dailyWage;
-                monthWage = (dailyWage * month)+monthWage;
-                totalWorkingHour=totalWorkingHour+FullDayHrs;
-                day=day+1;
+            switch (empCheck){
+                case 0:
+//                System.out.println(" Employee is Absent");
+                    day=day+1;
+                    break;
+                case 1:
+//                System.out.println("Employee is Present");
+                    dailyWage = WagePerHrs*FullDayHrs+dailyWage;
+                    monthWage = (dailyWage * month)+monthWage;
+                    totalWorkingHour=totalWorkingHour+FullDayHrs;
+                    day=day+1;
 //                System.out.println("Employee Daily Wage is :"+dailyWage);
 //                System.out.println("Employee Month Wage is :"+monthWage);
-                 break;
-            case 2:
-                System.out.println("Employee is Present but part time");
-                dailyWage = WagePerHrs*PartTimeHrs+dailyWage;
-                monthWage = dailyWage * month+monthWage;
-                totalWorkingHour=totalWorkingHour+PartTimeHrs;
-                day=day+1;
+                    break;
+                case 2:
+//                System.out.println("Employee is Present but part time");
+                    dailyWage = WagePerHrs*PartTimeHrs+dailyWage;
+                    monthWage = dailyWage * month+monthWage;
+                    totalWorkingHour=totalWorkingHour+PartTimeHrs;
+                    day=day+1;
 //                System.out.println("Employee Daily Wage is :"+dailyWage);
 //                System.out.println("Employee Month Wage is :"+monthWage);
-                break;
+                    break;
 
-        }}
+            }}
         System.out.println("                Day ,  :"+day+" "+totalWorkingHour);
         System.out.println("Employee Daily Wage is :"+dailyWage);
         System.out.println("Employee Month Wage is :"+monthWage);
-
-
     }
 
 }
