451efee0ecc7a4a3c3819989ceb5b1d56f646836
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-03-04 17:01:54 +0530

    Employee is present or absent

--- src/EmployeeWageComputation/EmployeeWageComputation.java
+++ src/EmployeeWageComputation/EmployeeWageComputation.java
@@ -1,7 +1,22 @@
 package EmployeeWageComputation;
 
+import java.util.Random;
+
 public class EmployeeWageComputation {
     public static void main(String[] args) {
+        System.out.println("   Welcome to Employee Wage Computation  ");
+
+        Random ran = new Random();
+        int empCheck = ran.nextInt(2);
+
+        switch (empCheck){
+            case 0:
+                System.out.println(" Employee is Absent");
+                break;
+            case 1:
+                System.out.println("Employee is Present");
+                 break;
+        }
 
     }
 
