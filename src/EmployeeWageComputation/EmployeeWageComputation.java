36f304fc8cf8c0721fc623546e0b1e06f4c2fc58
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-03-05 03:39:08 +0530

    Each Company Has its own wage

--- src/EmployeeWageComputation/EmployeeWageComputation.java
+++ src/EmployeeWageComputation/EmployeeWageComputation.java
@@ -5,29 +5,37 @@ import java.util.Random;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("   Welcome to Employee Wage Computation  ");
+//        int WagePerHrs,numberofWorkingDays,FullDayHrs(workingHourPer month)
 
-
-        Employee emp=new Employee();
+        Employee emp=new Employee("HCL",10,30,25);
         emp.calculate();
-
 
     }
 }
 
 class Employee{
     int WagePerHrs = 20;
-    int FullDayHrs = 8;
+    private int FullDayHrs = 8;
     int PartTimeHrs =4;
 
     int dailyWage=0;
     int month = 20;
     int monthWage = 20;
+
+    int workingHoursPerMonth=100;
+
+    public Employee(String comName ,int WagePerHrs,int FullDayHrs,int month){
+        this.WagePerHrs=    WagePerHrs;
+        this.FullDayHrs=    FullDayHrs;
+        this.workingHoursPerMonth     =    workingHoursPerMonth;
+
+    }
     public void calculate(){
 
         int totalWorkingHour=1 , day=1;
 
 
-        while (day<=20 || totalWorkingHour<=100){
+        while (day<=20 || totalWorkingHour<=workingHoursPerMonth){
             Random ran = new Random();
             int empCheck = ran.nextInt(3);
             switch (empCheck){
